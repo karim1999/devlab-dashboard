8032df36ef163cf534b5b8a2ec770cfff9354f3f
Karim Abdelgwad <karim.elbadry2@gmail.com>
2020-07-30 01:58:59 +0200

    Hide on condition

--- resources/views/sites/index.blade.php
+++ resources/views/sites/index.blade.php
@@ -1,10 +1,12 @@
 @extends('include.app')
 @section('content')
+    @if(isset($hide_menu) && $hide_menu)
     <style>
         .sub-right-nav {
             display: none;
         }
     </style>
+    @endif
     <div class="col-12 container d-flex " style="height: 100vh">
 
 		<h1 class="cairo text-center row justify-content-center align-self-center">برجاء إختيار الموقع</h1>
